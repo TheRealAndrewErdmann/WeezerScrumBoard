99717af76586ef89a6b167268e9aadd723e1ab77
Alexmdup <alexmdup@gmail.com>
2023-10-12 09:41:53 -0400

    fixed #2

---
+++ PMSoftwareFacade.java
@@ -0,0 +1,26 @@
+package WeezerScrumBoard;
+
+public class PMSoftwareFacade {
+
+    private User user;
+
+    public User login(String username, String password) {
+        return null;
+    }
+
+    public User signup(String username, String password,String email, String phoneNumber) {
+        return null;
+    }
+
+    public Project createProject(String title, String description) {
+        return null;
+    }
+
+    public void showAllProjects(){
+        
+    }
+
+
+
+
+}
