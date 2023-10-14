e862133f92aa83ecbcebb434b2f69650c9d0fe7b
shawnktan22 <shawnkier@outlook.com>
2023-10-14 15:49:36 -0400

    fixed #34

--- Column.java
+++ Column.java
@@ -1,3 +1,8 @@
+package WeezerScrumBoard;
+
+import java.util.ArrayList;
 public class Column {
-    
+    private String columnName;
+    private ArrayList<Task> tasks;
+    private ArrayList<String> status;
 }
