b3fe3a394e486d74a0ce0a0dd0b51447999231af
Alexmdup <alexmdup@gmail.com>
2023-10-14 14:23:41 -0400

    fixed #21

---
+++ Project.java
@@ -0,0 +1,13 @@
+package WeezerScrumBoard;
+import java.util.Map;
+import java.util.ArrayList;
+
+public class Project {
+
+    private String title;
+    private String description;
+    private Map<Role,User> participants;
+    private ArrayList<Column> columns;
+    private ArrayList<Comment> comments;
+    
+}
