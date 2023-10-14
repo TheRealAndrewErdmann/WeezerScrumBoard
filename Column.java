9e196886f4a7a62c76998fe6240f1c199f3d7492
shawnktan22 <shawnkier@outlook.com>
2023-10-14 15:51:39 -0400

    fixed #36-37

--- Column.java
+++ Column.java
@@ -1,8 +1,22 @@
 package WeezerScrumBoard;
 
+import java.io.ObjectInputFilter.Status;
 import java.util.ArrayList;
 public class Column {
     private String columnName;
     private ArrayList<Task> tasks;
     private ArrayList<String> status;
+
+    public Column(String name, Status status) {
+        columnName = name;
+
+    }
+
+    public boolean addTask(Task task) {
+        return false;
+    }
+
+    public boolean removeTask(Task task) {
+        return false;
+    }
 }
