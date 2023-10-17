8d27206912c68befe1b1af32753ef4d5b4b8c183
TheRealAndrewErdmann <andrewherdmann@gmail.com>
2023-10-17 09:16:15 -0400

    added JSON files to JSON

--- FileWriter.java
+++ FileWriter.java
@@ -13,6 +13,12 @@ public class FileWriter{
     }
 
     public void saveProjects(){
-
+        JSONObject userDetails = new JSONObject();
+		userDetails.put(USER_ID, user.getId().toString());
+		userDetails.put(USER_USER_NAME, user.getUserName());
+		userDetails.put(USER_FIRST_NAME, user.getFirstName());
+		userDetails.put(USER_LAST_NAME, user.getLastName());
+		userDetails.put(USER_AGE, user.getAge());
+		userDetails.put(USER_PHONE_NUMBER, user.getPhoneNumber())
     }
 }
