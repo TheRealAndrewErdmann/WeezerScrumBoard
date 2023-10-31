911b07465d2f9732d3d0f7199680af9b3b68b56a
Alexmdup <alexmdup@gmail.com>
2023-10-31 09:21:12 -0400

    fixed #67

--- Task.java
+++ Task.java
@@ -3,33 +3,55 @@ import java.util.UUID;
 
 public class Task {
     private UUID id;
+    private Project project;
     private String taskName;
     private String taskDescription;
     private Category category;
-    private String priority;
+    private Priority priority;
+    private String status;
     private ArrayList<Comment> comments;
     private ArrayList<Change> changes;
 
-    public Task(String taskName, String taskDescription, Category category, String priority){
+    public Task(Project project, String taskName, String taskDescription, Category category, Priority priority){
+        this.project = project;
         this.taskName = taskName;
         this.taskDescription = taskDescription;
         this.category = category;
         this.priority = priority;
+        comments = new ArrayList<Comment>();
+        changes = new ArrayList<Change>();
     }
     
-    public boolean changeStatus(String status){
-        return true;
+    public boolean changeStatus(String newStatus){
+        if(status == null)
+            return false;
+        return project.changeTaskStatus(this, status, newStatus);
     }
 
     public boolean changeDescription(String description){
+        if(description == null)
+            return false;
+        taskDescription = description;
         return true;
     }
 
     public boolean changePriority(Priority priority){
+        if(priority == null)
+            return false;
+        this.priority = priority;
         return true;
     }
 
     public boolean addComment(Comment comment){
+        if(comment == null)
+            return false;
+        return comments.add(comment);
+    }
+
+    public boolean setStatus(String status) {
+        if(status == null)
+            return false;
+        this.status = status;
         return true;
     }
 }
