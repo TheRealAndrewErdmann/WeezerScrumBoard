ae9083a65f9399679c80d550475fa77023f3792d
shawnktan22 <shawnkier@outlook.com>
2023-11-01 20:01:14 -0400

    added participants to txt file

--- AppUI.java
+++ AppUI.java
@@ -29,7 +29,12 @@ public class AppUI {
         System.out.println("----Viewing Electric Missiles----");
         PMSoftware.showAllColumns(found);
         Column abandoned = new Column("Abandoned", "Incomplete");
+        User Atticus = new User("Atticus", "Madden", "Amadden", "Madden", "amadden@gamil.com", "803-730-4872");
+        User Jeff = new User("Jeff", "Goldblum", "Jgoldblum", "Password", "jgoldblum@gamil.com", "803-826-9943");
+        User FInch = new User("Finch", "Finch", "Afinch", "Circle", "afinch@gamil.com", "803-730-4872");
         found.addColumn(abandoned);
+        found.addParticipant(Atticus, "Scrum Master");
+        found.addParticipant(Jeff, "Developer");
         Task cylinder = new Task(found, "Curve the metal to make a cylindrical shape",
                                  "description", Category.Feature, "High");
         Task initialize = new Task(found, "Initialize super algorithm to detonate at warp speed", 
@@ -40,8 +45,12 @@ public class AppUI {
         Comment avoid = new Comment("Atticus Madden", "Avoid civllians Jeff!");
         initialize.addComment(avoid);
         Comment reply = new Comment("Atticus Madden", "How about you do it jeff.");
-        //PMSoftware.findProject("Electrical Missiles").changeTaskStatus(cylinder, "To Do", "To Do");
+        PMSoftware.findProject("Electrical Missiles").changeTaskStatus(initialize, "To Do", "To Do");
         PMSoftware.findProject("Electrical Missiles").changeTaskStatus(cylinder, "To Do", "Doing");
+        Comment cylindrical = new Comment("Jeff Goldblum", "Not cyclindrical enough");
+        Comment question = new Comment("Atticus Finch", "What's a cylinder?");
+        cylinder.addComment(cylindrical);
+        cylinder.addComment(question);
         found.print();
     }
 
