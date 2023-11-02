f16fbdded20008281936455e8fb4121ae3130b29
yingdongfeng <yingdong@email.sc.edu>
2023-11-01 23:34:09 -0400

    Category

--- Category.java
+++ Category.java
@@ -1,6 +1,17 @@
+/**
+ * Enum representing different categories for software changes or tasks.
+ */
 public enum Category {
+    /** Represents a software defect or malfunction. */
     Bug,
+    
+    /** Represents a new feature or functionality to be added. */
     Feature,
+    
+    /** Represents tasks related to software documentation. */
     Documentation,
+    
+    /** Represents code restructuring without changing its external behavior. */
     Refactoring;
 }
+
