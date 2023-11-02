6e25335f5680ac3e688bca8d45e8919d690aceb2
yingdongfeng <yingdong@email.sc.edu>
2023-11-01 23:48:43 -0400

    Priority

--- Priority.java
+++ Priority.java
@@ -1,5 +1,13 @@
+/**
+ * Enum representing the priority levels of tasks or issues.
+ */
 public enum Priority {
-	High, 
-	Medium, 
-	Low;
+    /** Represents a high urgency level. */
+    High, 
+    
+    /** Represents a moderate urgency level. */
+    Medium, 
+    
+    /** Represents a low urgency level. */
+    Low;
 }
